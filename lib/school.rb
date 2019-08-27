105bc2beece200c7dece18f92dfd117a47a80613
Steven Laff <steve.laff@gmail.com>
2019-08-27 04:34:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,26 +10,36 @@ class School
   end
   
   def add_student(name, grade)
+    
     # Makes grade the roster key and sets the value to an array
     roster[grade] ||= []
+    
     # adds the students into the array keyed to their grade
     roster[grade] << name
   end
   
   def grade(is_in_grade)
-    # detects if a grade's 
+    
+    # returns the list of names in the specified grade
     roster.detect do |grade, names|
       if is_in_grade == grade
         return names
       end
     end
+    
   end
   
   def sort
+    
+    #create a new hash to return
     roster_hash = {}
+    
+    #
     roster.each do |grade, names|
       roster_hash[grade] = names.sort
     end
+    
+    #return the updated hash
     roster_hash
   end
   
